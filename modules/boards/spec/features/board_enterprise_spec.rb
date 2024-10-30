53555c1a1608d5e6b36d83cb2002d234b9ec5d8a
Behrokh Satarnejad <b.satarnejad@openproject.com>
2024-10-30 12:32:01 +0100

    fix failing test in board enterprise

--- modules/boards/spec/features/board_enterprise_spec.rb
+++ modules/boards/spec/features/board_enterprise_spec.rb
@@ -57,7 +57,7 @@ RSpec.describe "Boards enterprise spec", :js, :with_cuprite do
       page.find('[data-test-selector="add-board-button"]', text: "Board").click
 
       expect(page).to have_css("#{test_selector('op-tile-block')}:not(.-disabled)", text: "Basic")
-      expect(page).to have_css("#{test_selector('op-tile-block')}.-disabled", count: 5)
+      expect(page).to have_css("#{test_selector('op-tile-block')}.-disabled", count: 6)
     end
 
     it "shows a banner on the action board" do
@@ -85,7 +85,7 @@ RSpec.describe "Boards enterprise spec", :js, :with_cuprite do
     it "enables all options" do
       page.find('[data-test-selector="add-board-button"]', text: "Board").click
 
-      expect(page).to have_css("#{test_selector('op-tile-block')}:not(.-disabled)", count: 6)
+      expect(page).to have_css("#{test_selector('op-tile-block')}:not(.-disabled)", count: 7)
     end
 
     it "shows the action board" do
