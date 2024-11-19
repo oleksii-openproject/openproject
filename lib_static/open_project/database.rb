fc849f3669a28f3a1e026f19f21bd33bdc1d7468
Klaus Zanders <k.zanders@openproject.com>
2024-11-19 12:22:25 +0100

    use connection pool to check for pending migrations

--- lib_static/open_project/database.rb
+++ lib_static/open_project/database.rb
@@ -70,7 +70,7 @@ module OpenProject
       # Ensure cache is busted if result is positive or unset
       # and the value was cached
       if ensure_fresh || cached_result != false
-        fresh_result = connection.migration_context.needs_migration?
+        fresh_result = connection.pool.migration_context.needs_migration?
         Rails.cache.write(cache_key, expires_in: 1.hour)
         return fresh_result
       end
