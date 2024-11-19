b466fba10c157a45df0b5767d8c04ba95e5725b1
Kabiru Mwenja <k.mwenja@openproject.com>
2024-11-19 15:51:10 +0300

    [#59433] Define database model for reminders
    https://community.openproject.org/work_packages/59433

---
+++ db/migrate/20241119131205_create_reminders.rb
@@ -0,0 +1,12 @@
+class CreateReminders < ActiveRecord::Migration[7.1]
+  def change
+    create_table :reminders do |t|
+      t.references :remindable, polymorphic: true, null: false
+      t.references :creator, null: false, foreign_key: { to_table: :users }
+      t.datetime :remind_at, null: false
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
