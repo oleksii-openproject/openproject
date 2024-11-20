6e0947974de628781c993241901e07ef4a0784a2
Eric Schubert <e.schubert@openproject.com>
2024-11-20 14:55:53 +0100

    [#59391] applied PR comments

--- modules/storages/lib/api/v3/file_links/join_origin_status_to_file_links_relation.rb
+++ modules/storages/lib/api/v3/file_links/join_origin_status_to_file_links_relation.rb
@@ -1,5 +1,33 @@
 # frozen_string_literal: true
 
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
 module API
   module V3
     module FileLinks
@@ -7,8 +35,7 @@ module API
         # @param [Hash] id_status_map A hash mapping file link IDs to their origin status
         # in the format { 137: "view_allowed", 142: "error" }
         def self.create(id_status_map)
-          sanitized_sql = ActiveRecord::Base.send(
-            :sanitize_sql_array,
+          sanitized_sql = ActiveRecord::Base.sanitize_sql_array(
             [origin_status_join(id_status_map.size), *id_status_map.flatten]
           )
 
