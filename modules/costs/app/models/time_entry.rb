f0559f7399c12c839501d6ae6bee5305fd9b340c
Klaus Zanders <k.zanders@openproject.com>
2024-11-21 16:59:38 +0100

    add setting to enforce entering start and end times

--- modules/costs/app/models/time_entry.rb
+++ modules/costs/app/models/time_entry.rb
@@ -101,11 +101,17 @@ class TimeEntry < ApplicationRecord
       (user_id == usr.id && usr.allowed_in_project?(:view_own_hourly_rate, project))
   end
 
-  def self.can_track_start_and_end_time?(_project: nil)
-    OpenProject::FeatureDecisions.track_start_and_end_times_for_time_entries_active? &&
-    Setting.allow_tracking_start_and_end_times
+  class << self
+    def can_track_start_and_end_time?(_project: nil)
+      OpenProject::FeatureDecisions.track_start_and_end_times_for_time_entries_active? &&
+        Setting.allow_tracking_start_and_end_times?
+      # TODO: Add project check when we have decided if we also want a project specific flag
+    end
 
-    # TODO: Add project check when we have decided if we also want a project specific flag
+    def must_track_start_and_end_time?(_project: nil)
+      can_track_start_and_end_time? && Setting.enforce_tracking_start_and_end_times?
+      # TODO: Add project check when we have decided if we also want a project specific flag
+    end
   end
 
   private
