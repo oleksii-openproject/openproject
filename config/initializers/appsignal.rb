74c257145a85010509676cfcbf19a57a1fa96b94
Markus Kahl <machisuji@gmail.com>
2024-11-25 17:37:27 +0000

    Revert "Upgrade appsignal to 4.2"
    This reverts commit 8baabfaae96b2ca837f1cf23aa133b55d33aaf7a.

--- config/initializers/appsignal.rb
+++ config/initializers/appsignal.rb
@@ -3,53 +3,56 @@ require_relative "../../lib_static/open_project/appsignal"
 
 if OpenProject::Appsignal.enabled?
   require "appsignal"
-
   Rails.application.configure do |app|
+    config = {
+      active: true,
+      name: ENV.fetch("APPSIGNAL_NAME"),
+      push_api_key: ENV.fetch("APPSIGNAL_KEY"),
+      revision: OpenProject::VERSION.to_s,
+      ignore_actions: [
+        "OkComputer::OkComputerController#show",
+        "OkComputer::OkComputerController#index",
+        "GET::API::V3::Notifications::NotificationsAPI",
+        "GET::API::V3::Notifications::NotificationsAPI#/notifications/"
+      ],
+      ignore_errors: [
+        "Grape::Exceptions::MethodNotAllowed",
+        "ActionController::UnknownFormat",
+        "ActiveJob::DeserializationError",
+        "Net::SMTPServerBusy"
+      ],
+      ignore_logs: [
+        "GET /health_check"
+      ]
+    }
+
+    if ENV["APPSIGNAL_DEBUG"] == "true"
+      config[:log] = "stdout"
+      config[:debug] = true
+      config[:log_level] = "debug"
+    end
+
+    Appsignal.config = Appsignal::Config.new(
+      Rails.root,
+      Rails.env,
+      config
+    )
+
     app.middleware.insert_after(
       ActionDispatch::DebugExceptions,
       Appsignal::Rack::RailsInstrumentation
     )
-  end
 
-  Appsignal.configure do |config|
-    config.active = true
-    config.name = ENV.fetch("APPSIGNAL_NAME")
-    config.push_api_key = ENV.fetch("APPSIGNAL_KEY")
-    config.revision = OpenProject::VERSION.to_s
+    # Extend the core log delegator
+    handler = OpenProject::Appsignal.method(:exception_handler)
+    OpenProject::Logging::LogDelegator.register(:appsignal, handler)
 
-    if ENV["APPSIGNAL_DEBUG"] == "true"
-      config.log = "stdout"
-      config.log_level = "debug"
+    # Send our logs to appsignal
+    if OpenProject::Appsignal.logging_enabled?
+      appsignal_logger = Appsignal::Logger.new("rails")
+      Rails.logger.broadcast_to(appsignal_logger)
     end
 
-    config.ignore_actions = [
-      "OkComputer::OkComputerController#show",
-      "OkComputer::OkComputerController#index",
-      "GET::API::V3::Notifications::NotificationsAPI",
-      "GET::API::V3::Notifications::NotificationsAPI#/notifications/"
-    ]
-
-    config.ignore_errors = [
-      "Grape::Exceptions::MethodNotAllowed",
-      "ActionController::UnknownFormat",
-      "ActiveJob::DeserializationError",
-      "Net::SMTPServerBusy"
-    ]
-
-    config.ignore_logs = [
-      "GET /health_check"
-    ]
+    Appsignal.start
   end
-
-  # Extend the core log delegator
-  handler = OpenProject::Appsignal.method(:exception_handler)
-  OpenProject::Logging::LogDelegator.register(:appsignal, handler)
-
-  # Send our logs to appsignal
-  if OpenProject::Appsignal.logging_enabled?
-    appsignal_logger = Appsignal::Logger.new("rails")
-    Rails.logger.broadcast_to(appsignal_logger)
-  end
-
-  Appsignal.start
 end
