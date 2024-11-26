54b2f9a3489d30ac97fb11017a6005dbc0ea1801
Klaus Zanders <k.zanders@openproject.com>
2024-11-26 14:43:05 +0100

    remove end_time, add timezone

--- modules/costs/app/models/time_entry.rb
+++ modules/costs/app/models/time_entry.rb
@@ -47,21 +47,12 @@ class TimeEntry < ApplicationRecord
   validates_presence_of :hours, if: -> { !ongoing? }
   validates_numericality_of :hours, allow_nil: true, message: :invalid
 
-  validates :start_time, :end_time,
+  validates :start_time, :hours,
             presence: true,
             if: -> { TimeEntry.must_track_start_and_end_time? }
 
   validates :start_time,
             numericality: { only_integer: true, greater_than_or_equal_to: MIN_TIME, less_than_or_equal_to: MAX_TIME },
-            allow_blank: true
-
-  validates :end_time,
-            numericality: {
-              only_integer: true,
-              greater_than: ->(te) { te.start_time.to_i },
-              less_than_or_equal_to: MAX_TIME
-              # TODO: nice error message
-            },
             allow_blank: true
 
   scope :on_work_packages, ->(work_packages) { where(work_package_id: work_packages) }
@@ -129,10 +120,11 @@ class TimeEntry < ApplicationRecord
   end
 
   def end_timestamp
-    return nil if end_time.blank?
+    return nil if start_time.blank?
     return nil if time_zone.blank?
+    return nil if hours.blank?
 
-    ActiveSupport::TimeZone[time_zone].local(spent_on.year, spent_on.month, spent_on.day, end_time / 60, end_time % 60)
+    start_timestamp + hours.hours
   end
 
   class << self
